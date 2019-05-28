43b96e20f59b89f8645b2c17c3eb74fe9d8ad926
Renj <itrenjunhua@163.com>
2019-05-28 16:57:39 +0800

    修改BitmapUtils类

--- androidCommon/src/main/java/com/renj/common/utils/BitmapUtils.java
+++ androidCommon/src/main/java/com/renj/common/utils/BitmapUtils.java
@@ -1,13 +1,12 @@
 package com.renj.common.utils;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * ======================================================================
@@ -26,23 +25,102 @@ import java.io.IOException;
 public class BitmapUtils {
 
     /**
-     * 根据图片路径和目标宽高计算采样率
+     * 高效decode图片
      *
      * @param bitmapPath   原始图片路径
      * @param targetWidth  目标宽
      * @param targetHeight 目标高
-     * @return 采样率
+     * @return Bitmap
      */
-    public static int calculateSampleSize(@NonNull String bitmapPath, int targetWidth, int targetHeight) {
+    public static Bitmap decodeBitmap(@NonNull String bitmapPath, int targetWidth, int targetHeight) {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(bitmapPath, options);
-        int inSampleSize = calculateInSampleSize(options, targetWidth, targetHeight);
+        options.inSampleSize = calculateInSampleSize(options, targetWidth, targetHeight);
         options.inJustDecodeBounds = false;
-        return inSampleSize;
+        return BitmapFactory.decodeFile(bitmapPath, options);
     }
 
-    private static int calculateInSampleSize(BitmapFactory.Options options, int targetWidth, int targetHeight) {
+    /**
+     * 高效decode图片
+     *
+     * @param fileDescriptor {@link FileDescriptor}
+     * @param targetWidth    目标宽
+     * @param targetHeight   目标高
+     * @return Bitmap
+     */
+    public static Bitmap decodeBitmap(@NonNull FileDescriptor fileDescriptor, int targetWidth, int targetHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+        options.inSampleSize = calculateInSampleSize(options, targetWidth, targetHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+    }
+
+    /**
+     * 高效decode图片
+     *
+     * @param res          {@link Resources}
+     * @param resId        资源id
+     * @param targetWidth  目标宽
+     * @param targetHeight 目标高
+     * @return Bitmap
+     */
+    public static Bitmap decodeBitmap(@NonNull Resources res, @DrawableRes int resId, int targetWidth, int targetHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+        options.inSampleSize = calculateInSampleSize(options, targetWidth, targetHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    /**
+     * 高效decode图片
+     *
+     * @param data
+     * @param offSet
+     * @param length
+     * @param targetWidth  目标宽
+     * @param targetHeight 目标高
+     * @return Bitmap
+     */
+    public static Bitmap decodeBitmap(@NonNull byte[] data, int offSet, int length, int targetWidth, int targetHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeByteArray(data, offSet, length, options);
+        options.inSampleSize = calculateInSampleSize(options, targetWidth, targetHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeByteArray(data, offSet, length, options);
+    }
+
+    /**
+     * 高效decode图片
+     *
+     * @param inputStream
+     * @param targetWidth  目标宽
+     * @param targetHeight 目标高
+     * @return Bitmap
+     */
+    public static Bitmap decodeBitmap(@NonNull InputStream inputStream, int targetWidth, int targetHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(inputStream, null, options);
+        options.inSampleSize = calculateInSampleSize(options, targetWidth, targetHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeStream(inputStream, null, options);
+    }
+
+    /**
+     * 根据目标宽高计算采样率
+     *
+     * @param options
+     * @param targetWidth  目标宽
+     * @param targetHeight 目标高
+     * @return 采样率
+     */
+    public static int calculateInSampleSize(BitmapFactory.Options options, int targetWidth, int targetHeight) {
         int outWidth = options.outWidth;
         int outHeight = options.outHeight;
         if (targetWidth > outWidth && targetHeight > outHeight) {

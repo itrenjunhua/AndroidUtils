91cf0a15cc9a7a1988dc83e1dcbd5134634b40ad
Renj <itrenjunhua@163.com>
2019-07-19 02:11:42 +0800

    删除控件部分代码，只留下工具类代码

--- app/src/main/java/com/renj/test/MainActivity.kt
+++ app/src/main/java/com/renj/test/MainActivity.kt
@@ -2,42 +2,11 @@ package com.renj.test
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import com.renj.utils.common.UIUtils
-import com.renj.utils.weight.CircleImageView
-import com.renj.utils.weight.dialog.CustomDialog
 
 internal class MainActivity : AppCompatActivity() {
-    private lateinit var showDialog: Button
-    private lateinit var imageView: CircleImageView
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        showDialog = findViewById(R.id.bt_show_dialog)
-        imageView = findViewById(R.id.imageView)
-
-
-        imageView.setImageResource(R.mipmap.test)
-        showDialog.setOnClickListener {
-            CustomDialog.newInstance(this@MainActivity)
-                .isShowTitle(true)
-                .setTitleContent("自定义标题")
-                .setDialogContent("自定义对话框内容")
-                .setCancelText("取消")
-                .setConfirmText("确定")
-                .setCanceledOnTouchOut(false)
-                .setCustomDialogListener(object : CustomDialog.CustomDialogListener {
-                    override fun onCancel(dialog: CustomDialog) {
-                        UIUtils.showToastSafe("取消")
-                    }
-
-                    override fun onConfirm(dialog: CustomDialog) {
-                        UIUtils.showToastSafe("确定")
-                    }
-                })
-                .show()
-        }
     }
 }

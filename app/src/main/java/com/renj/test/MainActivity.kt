6ac44a673231e458099812221e9bc8b0ddbc9e22
Renj <itrenjunhua@163.com>
2019-11-29 14:08:47 +0800

    修改 NotificationUtils 类

--- app/src/main/java/com/renj/test/MainActivity.kt
+++ app/src/main/java/com/renj/test/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.renj.test
 
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
+import com.renj.utils.system.NotificationUtils
 
 internal class MainActivity : AppCompatActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val channelInfo1 = NotificationUtils.ChannelInfo("1", "aValue")
+        if (Build.VERSION.SDK_INT >= 26) {
+            NotificationUtils.getInstance().addChannel(channelInfo1)
+        }
+        NotificationUtils.getInstance().showNotification(
+            this, 1, channelInfo1,
+            R.mipmap.ic_launcher, 1, "aa",
+            "title", "content", null
+        )
     }
 }

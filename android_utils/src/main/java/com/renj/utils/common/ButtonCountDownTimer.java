8c989371a3f88b6b8e65b708178d64943abc5927
Renj <itrenjunhua@163.com>
2020-07-26 17:49:46 +0800

    完善 按钮倒计时器 类

--- android_utils/src/main/java/com/renj/utils/common/ButtonCountDownTimer.java
+++ android_utils/src/main/java/com/renj/utils/common/ButtonCountDownTimer.java
@@ -20,20 +20,45 @@ import android.widget.TextView;
  */
 public class ButtonCountDownTimer extends CountDownTimer {
     private TextView textView;
+    private boolean timeOnBefore;
     private String tickMessage;
     private String finishMessage;
 
     /**
      * @param millisInFuture    默认时长
      * @param countDownInterval 间隔时长
-     * @param textView            按钮控件
-     * @param tickMessage       倒计时时显示文案  "秒数 tickMessage"
+     * @param textView          按钮控件
+     * @param finishMessage     倒计时完成后显示文案
+     */
+    public ButtonCountDownTimer(long millisInFuture, long countDownInterval, @NonNull TextView textView, @NonNull String finishMessage) {
+        this(millisInFuture, countDownInterval, textView, "", finishMessage);
+    }
+
+    /**
+     * @param millisInFuture    默认时长
+     * @param countDownInterval 间隔时长
+     * @param textView          按钮控件
+     * @param tickMessage       倒计时时显示文案  "剩余秒数 tickMessage"
      * @param finishMessage     倒计时完成后显示文案
      */
     public ButtonCountDownTimer(long millisInFuture, long countDownInterval, @NonNull TextView textView,
                                 @NonNull String tickMessage, @NonNull String finishMessage) {
+        this(millisInFuture, countDownInterval, textView, true, tickMessage, finishMessage);
+    }
+
+    /**
+     * @param millisInFuture    默认时长
+     * @param countDownInterval 间隔时长
+     * @param textView          按钮控件
+     * @param timeOnBefore      时间显示在前面还是后面  true：前面（那么倒计时改变过程的显示为 "剩余秒数 tickMessage"）
+     * @param tickMessage       倒计时时显示文案
+     * @param finishMessage     倒计时完成后显示文案
+     */
+    public ButtonCountDownTimer(long millisInFuture, long countDownInterval, @NonNull TextView textView,
+                                boolean timeOnBefore, @NonNull String tickMessage, @NonNull String finishMessage) {
         super(millisInFuture, countDownInterval);
         this.textView = textView;
+        this.timeOnBefore = timeOnBefore;
         this.tickMessage = tickMessage;
         this.finishMessage = finishMessage;
     }
@@ -41,6 +66,9 @@ public class ButtonCountDownTimer extends CountDownTimer {
     public void startTimer() {
         textView.setEnabled(false);
         start();
+
+        if (onCountDownListener != null)
+            onCountDownListener.onStart();
     }
 
     public void cancelTimer() {
@@ -51,16 +79,99 @@ public class ButtonCountDownTimer extends CountDownTimer {
         cancel();
         textView.setEnabled(enable);
         textView.setText(cancelMessage);
+
+        if (onCountDownListener != null)
+            onCountDownListener.onCancel();
     }
 
     @Override
     public void onTick(long millisUntilFinished) {
-        textView.setText((millisUntilFinished / 1000) + tickMessage);
+        if (timeOnBefore)
+            textView.setText((millisUntilFinished / 1000) + tickMessage);
+        else
+            textView.setText(tickMessage + (millisUntilFinished / 1000));
+
+        if (onCountDownListener != null)
+            onCountDownListener.onTick(textView, millisUntilFinished);
     }
 
     @Override
     public void onFinish() {
         textView.setEnabled(true);
         textView.setText(finishMessage);
+
+        if (onCountDownListener != null)
+            onCountDownListener.onFinish();
     }
+
+
+    private OnCountDownListener onCountDownListener;
+
+    public void setOnCountDownListener(OnCountDownListener onCountDownListener) {
+        this.onCountDownListener = onCountDownListener;
+    }
+
+    /**
+     * 各种状态监听
+     */
+    public interface OnCountDownListener {
+        /**
+         * 开始倒计时
+         */
+        void onStart();
+
+        /**
+         * 正在倒计时
+         *
+         * @param textView            控件
+         * @param millisUntilFinished 剩余时间 毫秒
+         */
+        void onTick(TextView textView, long millisUntilFinished);
+
+        /**
+         * 取消倒计时
+         */
+        void onCancel();
+
+        /**
+         * 完成倒计时
+         */
+        void onFinish();
+    }
+
+    public static class SimpleCountDown implements OnCountDownListener {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onStart() {
+
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onTick(TextView textView, long millisUntilFinished) {
+
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onCancel() {
+
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onFinish() {
+
+        }
+    }
+
 }
